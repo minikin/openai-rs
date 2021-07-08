f457c01b1d7026da09f5a27916692a27ee356a8f
minikin <djminikin@gmail.com>
2021-07-08 23:16:59 +0200

    Add Sampling

---
+++ src/supporting_types/sampling.rs
@@ -0,0 +1,16 @@
+/// The sampling method used by a model in completing a request.
+#[derive(Debug, Eq, PartialEq, Clone)]
+pub enum Sampling {
+    // Sampling temperature.
+    //
+    // Higher values means the model will take more risks.
+    // Try 0.9 for more creative applications,
+    // and 0 (argmax sampling) for ones with a well-defined answer.
+    Temperature(f32)
+
+    // Nucleus sampling.
+    //
+    // The model considers the results of the tokens with top_p probability mass.
+    // So 0.1 means only the tokens comprising the top 10% probability mass are considered.
+    Nucleus(f64)
+}
