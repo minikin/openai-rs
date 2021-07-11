deb6d6cda33041accc10d613d8e21c751de6aae4
minikin <djminikin@gmail.com>
2021-07-11 22:26:23 +0200

    Add Answer

---
+++ src/response_types/answer.rs
@@ -0,0 +1,27 @@
+use std::{collections::HashMap, fmt::Display};
+use serde::{Deserialize, Serialize}
+
+/// The result of a question answering request.
+///
+/// Given a question, a set of documents, and some examples,
+/// the API generates an answer to the question
+/// based on the information in the set of documents.
+/// This is useful for question-answering applications on sources of truth,
+/// like company documentation or a knowledge base.
+#[derive( Deserialize, Clone,  Debug,)]
+pub struct Answer {
+     /// The completion for the answer.
+    pub completion: String
+
+    /// The possible answers.
+    pub answers: Vec<String>
+
+    /// The engine used to answer the question
+    pub engine: EngineId
+
+    /// The engine used for searching.
+    pub search_engine: EngineId
+
+    /// The documents selected for each answer.
+    pub selectedDocuments: HashMap<i64, String>,
+}
